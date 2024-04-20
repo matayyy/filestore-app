29b5e79806d949935b112f418d1ad10fa6bc34e3
matay <mat.mielewczyk@gmail.com>
2024-04-20 22:30:39 +0200

    initial commit

---
+++ src/test/java/com/matay/filestoreapp/FilestoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matay.filestoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilestoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
